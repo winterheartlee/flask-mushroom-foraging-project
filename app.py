ea2eb433aa9b324cb340c1232a41d1f7a9faa3e9
winterheartlee <leejonhouse@gmail.com>
2021-12-06 23:46:47 +0000

    Nav bar added and register page created and linked

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -18,14 +19,20 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template('mushrooms.html')
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+        port=int(os.environ.get("PORT")),
+        debug=True)
 
 
 """
